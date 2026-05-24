308de9b257c6459003eb61c06a825b67747ae3a7
aranya-jana <aranyajana01@gmail.com>
2026-05-24 22:18:10 +0530

    Update the code for loading model

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def get_requirements()->List[str]:
 print(get_requirements())
 
 setup(
-    name="AI-TRAVEL-PLANNER",
+    name="AI-TRIP-PLANNER",
     version="0.0.1",
     author="Aranya Jana",
     author_email="aj.coder05@gmail.com",
