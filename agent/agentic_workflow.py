984b5d8e1c4e5deb63e983b801e9b313da2ae7e3
aranya-jana <aranyajana01@gmail.com>
2026-05-26 23:26:37 +0530

    update agent graph

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -23,7 +23,15 @@ class GraphBuilder():
 
 
     def build_graph(self):
-        pass
+        graph_builder=StateGraph(MessagesState)
+        graph_builder.add_node("agent", self.agent_function)
+        graph_builder.add_node("tools", ToolNode(tools=self.tools))
+        graph_builder.add_edge(START,"agent")
+        graph_builder.add_conditional_edges("agent",tools_condition)
+        graph_builder.add_edge("tools","agent")
+        graph_builder.add_edge("agent",END)
+        self.graph = graph_builder.compile()
+        return self.graph
 
 
 
